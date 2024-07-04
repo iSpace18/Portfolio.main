1d4e2958ae2d240bfc4d4433cb22d6be0c813f9a
iSpace18 <steamdavid2007@gmail.com>
2024-07-04 19:30:20 +0300

    314134

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Shnazaryan David </span>
+            Hi Everyone, I am <span className="purple">Shahnazaryan David </span>
             from <span className="purple">Sochi, Russia.</span>
             <br />
             I am currently studying at the Faculty of Applied Computer Science.
